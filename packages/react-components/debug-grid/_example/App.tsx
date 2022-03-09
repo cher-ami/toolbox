b537cc8e40943b53e59d83454281076ecb57539d
Sam Phlix <sam@cher-ami.tv>
2022-03-09 16:40:20 +0100

    Refacto

--- packages/react-components/debug-grid/_example/App.tsx
+++ packages/react-components/debug-grid/_example/App.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import DebugGrid from "../debugGrid/DebugGrid";
 
 const App = () => {
   return (
     <div>
+      <DebugGrid columnsDesktop={40} columnsTablet={28} columnsMobile={20} maxSize={1440} triggerKey={"Space"} />
     </div>
   );
 };
--- packages/react-components/debug-grid/debugGrid/DebugGrid.tsx
+++ packages/react-components/debug-grid/debugGrid/DebugGrid.tsx
@@ -1,17 +1,43 @@
 import css from "./DebugGrid.module.less"
 import React, { useEffect, useState } from "react"
-import { merge } from "../../helpers/merge"
-import debug from "@wbe/debug"
 
 interface IProps {
-  className?: string
+  /**
+   * Number of column on desktop (resolutions over @breakpoint-laptop declared in breakpoints.less)
+   * Type: number
+   */
   columnsDesktop: number
+  /**
+   * Number of column on tablet (resolutions over @breakpoint-tablet declared in breakpoints.less)
+   * Type: number
+   */
   columnsTablet: number
+  /**
+   * Number of column on mobile (resolutions under @breakpoint-tablet declared in breakpoints.less)
+   * Type: number
+   */
   columnsMobile: number
+  /**
+   * Max resolution : If setted, over this resolution the grid will stop growing proportionnaly and columns width will be setted in px
+   * Type: number
+   * Default: undefined
+   */
+  maxSize?: number
+  /**
+   * The color of the lines
+   * Type: string (hex code)
+   * Default: "#f323fa" (pink)
+   */
+  color?: string
+  /**
+   * The key that will display the grid if it is pressed
+   * Type: string
+   * Default: g
+   */
+  triggerKey?: 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' 
+  | 'l' | 'm' | 'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x' 
+  | 'y' | 'z' | 'Space' | 'Enter'
 }
-
-const componentName = "DebugGrid"
-const log = debug(`front:${componentName}`)
 
 /**
  * @name DebugGrid
@@ -20,9 +46,8 @@ function DebugGrid(props: IProps) {
   const [showGrid, setShowGrid] = useState<boolean>(false)
 
   useEffect(() => {
-    if (import.meta.env.VITE_SHOW_GRID != "true") return
-    const keyHandler = (e) => {
-      if (e.key === "g") {
+    const keyHandler = (e) => {            
+      if (e.key === (props.triggerKey?.replace('Space', ' ') || "g")) {
         setShowGrid(!showGrid)
       }
     }
@@ -34,18 +59,25 @@ function DebugGrid(props: IProps) {
   }, [showGrid])
 
   return (
-    <div className={merge([css.root, props.className])}>
+    <div className={css.root}>
       {showGrid &&
         Array.from(Array(props.columnsDesktop + 1)).map((el, i) => {
           return (
             <div
-              className={merge([
+              className={[
                 css.line,
                 i > props.columnsMobile + 1 && css.lineDesktop,
                 i > props.columnsTablet + 1 && css.lineTablet,
-              ])}
+              ].filter((e) => e).join(" ")}
+              style={{
+                ["--mobile-width" as string]: 100 / props.columnsMobile + "vw",
+                ["--tablet-width" as string]: 100 / props.columnsTablet + "vw",
+                ["--laptop-width" as string]: 100 / props.columnsDesktop + "vw",
+                ["--bigLaptop-width" as string]: props.maxSize ? `${props.maxSize / props.columnsDesktop}px` : `${100 / props.columnsDesktop}vw`,
+                ["--color" as string]: /^#([0-9A-F]{3}){1,2}$/i.test(props.color || null) ? props.color : "#f323fa",
+              }}
               key={i}
-            ></div>
+            />
           )
         })}
     </div>
