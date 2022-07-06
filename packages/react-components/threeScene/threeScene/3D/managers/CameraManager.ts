c9b3b7f642bb6f2322b410460cabe27c20ea5e18
Bastien Cornier <bastien@cher-ami.tv>
2022-07-06 16:49:31 +0200

    Refacto camera controls modes more robust switch

--- packages/react-components/threeScene/threeScene/3D/managers/CameraManager.ts
+++ packages/react-components/threeScene/threeScene/3D/managers/CameraManager.ts
@@ -1,4 +1,4 @@
-import CameraControls from "camera-controls";
+import CameraControls from "camera-controls"
 import {
   MOUSE,
   Vector2,
@@ -13,8 +13,7 @@ import {
   MathUtils,
   PerspectiveCamera,
   OrthographicCamera,
-} from "three";
-import sceneConfig from "../data/sceneConfig";
+} from "three"
 
 // Get only what we need for camera controls
 const subsetOfTHREE = {
@@ -32,10 +31,10 @@ const subsetOfTHREE = {
     DEG2RAD: MathUtils.DEG2RAD,
     clamp: MathUtils.clamp,
   },
-};
+}
 
-export type TCameraControlMode = "orbit" | "firstPerson";
-const EPS = 1e-5;
+export type TCameraControlMode = "orbit" | "firstPerson"
+const EPS = 1e-5
 
 // TODO: rajouter un click sur un objet mouvement camera
 /**
@@ -43,28 +42,30 @@ const EPS = 1e-5;
  * @class CameraControler
  */
 class CameraManager {
-  camera: PerspectiveCamera | OrthographicCamera;
-  rendererDomContainer: HTMLElement;
-  controls: CameraControls;
-  hasControlsUpdated: boolean;
-  basePosition: Vector3;
+  camera: PerspectiveCamera | OrthographicCamera
+  rendererDomContainer: HTMLElement
+  controls: CameraControls
+  hasControlsUpdated: boolean
+  basePosition: Vector3
 
-  private _mode: TCameraControlMode;
+  isUserControlling: boolean = false
+
+  private _mode: TCameraControlMode
   set mode(mode: string) {
-    this.setMode(mode);
-    this.mode = mode;
+    this.setMode(mode)
+    this.mode = mode
   }
   get mode(): TCameraControlMode {
-    return this._mode;
+    return this._mode
   }
 
-  private _enabled: boolean = true;
+  private _enabled: boolean = true
   set enabled(enabled: boolean) {
-    this.controls.enabled = enabled;
-    this._enabled = enabled;
+    this.controls.enabled = enabled
+    this._enabled = enabled
   }
   get enabled(): boolean {
-    return this._enabled;
+    return this._enabled
   }
 
   constructor(
@@ -72,77 +73,91 @@ class CameraManager {
     rendererDomContainer: HTMLElement,
     mode: TCameraControlMode = "orbit"
   ) {
-    this.camera = camera;
-    this.rendererDomContainer = rendererDomContainer;
+    this.camera = camera
+    this.rendererDomContainer = rendererDomContainer
 
-    CameraControls.install({ THREE: subsetOfTHREE });
+    CameraControls.install({ THREE: subsetOfTHREE })
 
-    this.basePosition = this.camera.position.clone();
+    this.basePosition = this.camera.position.clone()
 
     // in order to archive FPS look, set EPSILON for the distance to the center
-    this.camera.position.set(0, 0, EPS);
+    this.camera.position.set(0, 0, EPS)
 
-    this.controls = new CameraControls(this.camera, this.rendererDomContainer);
+    this.controls = new CameraControls(this.camera, this.rendererDomContainer)
 
-    this.setMode(mode);
+    this.setMode(mode)
+
+    this.controls.addEventListener("controlstart", () => (this.isUserControlling = true))
+    this.controls.addEventListener("controlend", () => (this.isUserControlling = false))
 
     // TODO: temp
-    window["__CAM_CONTROLS__"] = this;
+    window["__CAM_CONTROLS__"] = this
   }
 
-  setMode(mode: string) {
-    this.controls.reset();
+  setMode(mode: string, moveCamera: boolean = true) {
+    this.controls.reset()
     if (mode === "orbit") {
-      this.setupModeOrbit();
+      this.setupModeOrbit(moveCamera)
     } else if (mode === "firstPerson") {
-      this.setupModeFirstPerson();
+      this.setupModeFirstPerson(moveCamera)
     }
   }
 
-  setupModeOrbit() {
-    this.controls.minDistance = 0;
-    this.controls.maxDistance = Infinity;
-    this.controls.azimuthRotateSpeed = 1;
-    this.controls.polarRotateSpeed = 1;
-    this.controls.truckSpeed = 2;
-    this.controls.mouseButtons.wheel = CameraControls.ACTION.DOLLY;
-    this.controls.mouseButtons.right = CameraControls.ACTION.NONE;
+  setupModeOrbit(moveCamera: boolean = true) {
+    this.controls.minAzimuthAngle = -Infinity
+    this.controls.maxAzimuthAngle = Infinity
+    this.controls.minDistance = 0
+    this.controls.maxDistance = Infinity
+    this.controls.azimuthRotateSpeed = 1
+    this.controls.polarRotateSpeed = 1
+    this.controls.truckSpeed = 2
+    this.controls.mouseButtons.wheel = CameraControls.ACTION.DOLLY
+    this.controls.mouseButtons.right = CameraControls.ACTION.NONE
 
-    this.controls.moveTo(
-      this.basePosition.x,
-      this.basePosition.y,
-      this.basePosition.z,
-      false
-    );
-    this.controls.setTarget(0, 0, EPS);
+    if (moveCamera) {
+      this.controls.moveTo(
+        this.basePosition.x,
+        this.basePosition.y,
+        this.basePosition.z,
+        false
+      )
+      this.controls.setTarget(0, 0, EPS)
+    }
 
-    this.controls.saveState();
+    this.controls.saveState()
   }
 
-  setupModeFirstPerson() {
-    this.controls.minDistance = this.controls.maxDistance = 1;
-    this.controls.azimuthRotateSpeed = -0.3; // negative value to invert rotation direction
-    this.controls.polarRotateSpeed = -0.3; // negative value to invert rotation direction
-    this.controls.truckSpeed = 10;
-    this.controls.mouseButtons.wheel = CameraControls.ACTION.ZOOM;
-    this.controls.mouseButtons.right = CameraControls.ACTION.NONE;
-    this.controls.touches.two = CameraControls.ACTION.TOUCH_ZOOM_TRUCK;
+  setupModeFirstPerson(moveCamera: boolean = true, enableTransitions: boolean = true) {
+    if (moveCamera) {
+      this.controls.minDistance = 0
+      this.controls.reset(enableTransitions)
+      this.controls.moveTo(0, 0, EPS, enableTransitions)
+      this.controls.setTarget(0, 0, 0, enableTransitions)
+      this.controls.dollyTo(EPS, enableTransitions)
+    }
 
-    this.controls.moveTo(0, 0, EPS, false);
-    this.controls.setTarget(0, 0, 0);
+    this.controls.minAzimuthAngle = -Infinity
+    this.controls.maxAzimuthAngle = Infinity
+    this.controls.minDistance = this.controls.maxDistance = 1
+    this.controls.azimuthRotateSpeed = -0.3 // negative value to invert rotation direction
+    this.controls.polarRotateSpeed = -0.3 // negative value to invert rotation direction
+    this.controls.truckSpeed = 10
+    this.controls.mouseButtons.wheel = CameraControls.ACTION.ZOOM
+    this.controls.mouseButtons.right = CameraControls.ACTION.NONE
+    this.controls.touches.two = CameraControls.ACTION.TOUCH_ZOOM_TRUCK
 
-    this.controls.saveState();
+    this.controls.saveState()
   }
 
   loop(deltaTime: number) {
-    if (!this._enabled) return;
+    if (!this._enabled) return
     // update camera controls
-    this.hasControlsUpdated = this.controls.update(deltaTime);
+    this.hasControlsUpdated = this.controls.update(deltaTime)
   }
 
   dispose() {
-    this.controls.dispose();
+    this.controls.dispose()
   }
 }
 
-export default CameraManager;
+export default CameraManager
