836389f1a8820a6e635db46748b92453ff25fcfd
Bastien Cornier <bastien@cher-ami.tv>
2022-07-06 16:51:15 +0200

    Remove types from assets data

--- packages/react-components/threeScene/_example/assets3dData.ts
+++ packages/react-components/threeScene/_example/assets3dData.ts
@@ -6,20 +6,16 @@ import footprintCourt from "./assets/footprint_court.hdr?url";
 // Move to data folder for example
 const assets3dData: IFile[] = [
   {
-    // TODO: add local files
     path: damagedHelmet,
     id: "model-test",
-    type: EFileType.GLTF,
   },
   {
     path: footprintCourt,
     id: "envmap",
-    type: EFileType.HDR,
   },
   {
     path: testBasisTexture,
     id: "test-basis-texture",
-    type: EFileType.KTX2,
   },
 ];
 
